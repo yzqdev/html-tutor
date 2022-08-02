de94a678c082e9c4b1657ec8b37da375f4e4658c
yzqdev <yzqdev@outlook.com>
2022-08-02 22:51:47 +0800

    add route

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,8 @@ import NotFound from '@/components/NotFound.vue'
 import Home from '@/views/Home.vue'
 import HomeMain from '@/views/HomeMain.vue'
 import CssTwo from '@/views/cssLayout/Two.vue'
+import CssLayout from '@/views/cssLayout/Main.vue'
+import Flex from '@/views/cssLayout/Flex.vue'
 let routes: RouteRecordRaw[] = [
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
   { path: '/home', redirect: '/home/main' },
@@ -29,6 +31,52 @@ let routes: RouteRecordRaw[] = [
     path:'/css-two',
     name:'CssTwo',
     component:CssTwo
+  },{
+    path:'/css-layout',
+    component:CssLayout,
+    children:[
+      {
+        path:'flex',
+        name:'flex',
+        component:Flex
+      },{
+        path:'flow',
+        name:'flow',
+        component: ()=>import('@/views/cssLayout/FlowLayout.vue')
+      },{
+        path:'inline',
+        name:'inline',
+        component: ()=>import('@/views/cssLayout/Inline.vue')
+      },{
+        path:'relative',
+        name:'relative',
+        component: ()=>import('@/views/cssLayout/RelativeLayout.vue')
+      },{
+        path:'two',
+        name:'two',
+        component: ()=>import('@/views/cssLayout/Two.vue')
+      },{
+        path:'menu',
+        name:'MenuLayout',
+        component: ()=>import('@/views/cssLayout/MenuLayout.vue')
+      },{
+        path:'pin',
+        name:'Pin',
+        component: ()=>import('@/views/cssLayout/Pin.vue')
+      },{
+        path:'verticle',
+        name:'Vertivle',
+        component: ()=>import('@/views/cssLayout/Verticle.vue')
+      },{
+        path:'awesome',
+        name:'Awesome',
+        component: ()=>import('@/views/cssLayout/Awesome.vue')
+      },{
+        path:'temp',
+        name:'Temp',
+        component: ()=>import('@/views/cssLayout/Temp.vue')
+      },
+    ]
   }
 ]
 
--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -7,10 +7,21 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    Bfc: typeof import('./../components/css/bfc.vue')['default']
+    Borderbox: typeof import('./../components/css/borderbox.vue')['default']
+    Bubble: typeof import('./../components/css/bubble.vue')['default']
+    Clearfix: typeof import('./../components/clearfix.vue')['default']
+    Effect: typeof import('./../components/css/effect.vue')['default']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
+    Gentable: typeof import('./../components/css/Gentable.vue')['default']
     Hello: typeof import('./../components/Hello.vue')['default']
+    Holyflex: typeof import('./../components/css/Holyflex.vue')['default']
+    Holygrid: typeof import('./../components/css/Holygrid.vue')['default']
+    Layout: typeof import('./../components/css/layout.vue')['default']
+    NewVue: typeof import('./../components/css/NewVue.vue')['default']
     NotFound: typeof import('./../components/NotFound.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Triange: typeof import('./../components/css/triange.vue')['default']
   }
 }
